e319ffba1a4b04b758bebd09c39784a4b4337ff0
Nicolas JOURDEN <nicolas.jourden@laposte.net>
2018-02-03 12:10:48 +0100

    Changed script for ffmpeg.

--- video/mov2mp4.sh
+++ video/mov2mp4.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 
-find $1 -name '*.MOV' -print0 | while IFS= read -r -d $'\0' FILE; do 
-  echo " --- $FILE --- "
-  ffmpeg -i "$FILE" -c:v h264 -c:a aac -b:a 160k -strict -2 -preset veryslow -crf 20 "${FILE/MOV/mp4}"
-done
+find $1 -name '*.MOV' -exec bash -c 'ffmpeg -i "$0" -c:v h264 -c:a aac -b:a 160k -strict -2 -crf 20 "${0/MOV/mp4}"' {} \;
+
 
