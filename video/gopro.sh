879661215441ce69592fcb3e20b47a695542e38c
Nicolas JOURDEN <nicolas.jourden@laposte.net>
2018-08-12 18:14:34 +0200

    Added option to get track of time (creation date).

--- video/gopro.sh
+++ video/gopro.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+FILES=($(ls -1 $1/*.MP4))
+TIMESTAMP=$(stat -c %y ${FILES[0]})
 TMP=$(mktemp -p $1)
+
 for FILE in $(ls -1 $1/*.MP4); do
   FL=$(basename $FILE)
   echo "$FILE"
@@ -10,3 +13,5 @@ done
 cd $1
 ffmpeg -f concat -i $TMP -c copy $1/output.mp4
 rm -v $TMP
+
+touch --date="$TIMESTAMP" $1/output.mp4
