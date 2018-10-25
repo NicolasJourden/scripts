fc41453bb447dab68ae8da44abaacc825fac01c1
Nicolas JOURDEN <nicolas.jourden@laposte.net>
2018-10-25 05:47:40 +0200

    Added several quality option output.

--- video/gopro.sh
+++ video/gopro.sh
@@ -11,7 +11,13 @@ for FILE in $(ls -1 $1/*.MP4); do
 done
 
 cd $1
-ffmpeg -f concat -i $TMP -c copy $1/output.mp4
+ffmpeg -f concat -i $TMP -an -c copy $1/output_tmp.mp4
 rm -v $TMP
+touch --date="$TIMESTAMP" $1/output_tmp.mp4
 
-touch --date="$TIMESTAMP" $1/output.mp4
+# Compress:
+ffmpeg -i "$1/output_tmp.mp4" -c:v h264 -strict -2 -crf 20 $1/$(basename $1)_crf20.mp4
+ffmpeg -i "$1/output_tmp.mp4" -c:v h264 -strict -2 -crf 23 $1/$(basename $1)_crf23.mp4
+ffmpeg -i "$1/output_tmp.mp4" -c:v h264 -strict -2 -crf 35 $1/$(basename $1)_crf35.mp4
+ffmpeg -i "$1/output_tmp.mp4" -c:v h264 -strict -2 -crf 50 $1/$(basename $1)_crf50.mp4
+touch --date="$TIMESTAMP" $1/$(basename $1)*.mp4
